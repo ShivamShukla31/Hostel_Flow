d25f8d2037b7dcfb7b6e0cca4645713d0a7f3cf9
Shivam Shukla <shuklashivam3101@gmail.com>
2026-05-27 18:48:17 +0530

    chore: remove duplicate archived backend copy from repo

--- Archived_Backend_Root/utils/generateTicketId.js
+++
@@ -1,7 +0,0 @@
-import { nanoid } from "nanoid";
-
-const generateTicketId = () => {
-    return `HTL-${new Date().getFullYear()}-${nanoid(6).toUpperCase()}`;
-};
-
-export default generateTicketId;
