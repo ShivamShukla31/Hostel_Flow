0c88ec51f1a9f787b9a8d4dc88cee86dc2649c74
Shivam Shukla <shuklashivam3101@gmail.com>
2026-05-28 00:15:48 +0530

    chore: simplify allowed origins for CORS configuration

--- Backend/index.js
+++ Backend/index.js
@@ -20,15 +20,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const ALLOWED_ORIGINS = [
-  'http://localhost:5173',
-  'http://localhost:5174',
-  'http://localhost:5175',
-  'http://localhost:5176',
-  'http://127.0.0.1:5173',
-  'http://127.0.0.1:5174',
-  'http://127.0.0.1:5175',
-  'http://127.0.0.1:5176',
-];
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  process.env.CLIENT_URL,
+].filter(Boolean);
 
 const corsOptions = {
   origin: (origin, callback) => {
